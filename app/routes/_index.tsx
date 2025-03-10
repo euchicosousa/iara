7d8d167902f119545d482e7e539929b0b3cc7fef
Chico Sousa <ochicosousa@gmail.com>
2025-03-09 23:09:51 -0300

    start

---
+++ app/routes/_index.tsx
@@ -0,0 +1,75 @@
+import { Link } from "@remix-run/react";
+import { MetaFunction } from "@vercel/remix";
+
+export const meta: MetaFunction = () => {
+	return [
+		{ title: "Dra. Iara Tomaz - Gonecologista e Obstetra" },
+		{
+			name: "description",
+			content:
+				"Médica ginecologista e obstetra em Sobral e região norte. Atendimento humanizado e personalizado.",
+		},
+	];
+};
+
+export default function Index() {
+	return (
+		<div className="w-full h-screen grid place-content-center bg-iara">
+			<div className="p-8 rounded-2xl mx-auto flex flex-col justify-center items-center gap-4 border-4">
+				<div className="rounded-full overflow-hidden size-48 -mt-32 border-4 border-white">
+					<img src="./iara.jpg" alt="" />
+				</div>
+				{/* link */}
+				<div className="flex flex-col mt-8 gap-2 mx-auto w-full">
+					<Link
+						to={encodeURI(
+							"https://wa.me/5588981563429?text=Olá, gostaria de agendar uma consulta com a Dra. Iara Tomaz."
+						)}
+						target="_blank"
+						rel="noreferrer"
+						className="link"
+					>
+						<span>Vila Clinic </span>
+						<span className="uppercase text-sm tracking-wider">
+							Sobral
+						</span>
+					</Link>
+					<Link
+						to={encodeURI(
+							"https://wa.me/5588981563429?text=Olá, gostaria de agendar uma consulta com a Dra. Iara Tomaz."
+						)}
+						target="_blank"
+						rel="noreferrer"
+						className="link"
+					>
+						<span>SmartMed </span>
+						<span className="uppercase text-sm tracking-wider">
+							Massapê
+						</span>
+					</Link>
+					<Link
+						to={encodeURI(
+							"https://wa.me/5588981563429?text=Olá, gostaria de agendar uma consulta com a Dra. Iara Tomaz."
+						)}
+						target="_blank"
+						rel="noreferrer"
+						className="link"
+					>
+						<span>Laboclisa</span>
+						<span className="uppercase text-sm tracking-wider">
+							Santana
+						</span>
+					</Link>
+				</div>
+				<img src="./logo.svg" alt="" className="h-24 mt-12" />
+				{/* Social */}
+
+				{/* Copyrights */}
+				{/* <div className="text-xs opacity-50 pb-8  text-center">
+					© {new Date().getFullYear()} PARK - Sua aventura culinária
+					em Sobral
+				</div> */}
+			</div>
+		</div>
+	);
+}
---
+++ app/routes/happyhour.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+
+export default function HappyHour() {
+	useEffect(() => {
+		location.href =
+			"https://parkchoppsobral.goomer.app/menu?hash=2eNZw#1498899";
+	}, []);
+
+	return (
+		<div className="grid w-screen text-center h-screen place-content-center p-8 text-sobral bg-iara">
+			<div className="mb-8">
+				Estamos te levando <br /> para o nosso{" "}
+				<strong>CARDÁPIO...</strong>
+			</div>
+			<img
+				src="/loader.png"
+				alt="Estamos te levando para o nosso cardápio..."
+				className="animate-spin mx-auto w-12"
+			/>
+		</div>
+	);
+}
